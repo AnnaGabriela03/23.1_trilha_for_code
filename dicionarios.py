2ffaddabc6591ced61e310c04b980a15eabc5d3e
Anna Gabriela <145726984+AnnaGabriela03@users.noreply.github.com>
2023-09-30 01:15:43 +0000

    aula do dia 29

---
+++ dicionarios.py
@@ -0,0 +1,5 @@
+dictionary = {"Bairros": ["Barra da Tijuca", "Gávea", "Ipanema","Leblon"],
+              "Cidades":["Rio de Janeiro", "São Paulo", "Belo Horizonte"]}
+dictionary["Estados"] = ["RJ", "SP", "MG"]
+# print(dictionary["Bairros"])
+print(dictionary.values())
---
+++ sets_e_tuplas.py
@@ -0,0 +1,11 @@
+# lista = [1,2,3,3,4,5,5]
+# conjunto = {1,2,3,3,4,5,5}
+# conjunto2 = set(lista)
+
+# print(conjunto2)
+
+tupla = (1,2)
+um, dois = tupla
+
+print(um)
+print(tupla)
